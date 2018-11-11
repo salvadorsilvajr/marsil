eebf43eba7739aeceb6fc1debf2d0ed0e837fd5b
Salvador Silva <salvadorsilvajr@yahoo.com>
2018-11-11 21:52:52 +0000

    inicial commit

---
+++ app.js
@@ -0,0 +1,21 @@
+var express         = require("express"),
+    app             = express(), 
+    bodyParser      = require("body-parser"),
+    // mongoose        = require("mongoose"),
+    serveStatic     = require('serve-static'),
+    nodemailer      = require('nodemailer')
+    
+app.use(express.static('public'))     
+// mongoose.connect("mongodb://localhost/marsil", {useNewUrlParser: true});
+app.use (bodyParser.urlencoded({extended: true}));
+app.set("view engine", "ejs");
+
+
+app.get("/", function(req, res){
+   res.render("index");
+});
+
+
+app.listen(process.env.PORT, process.env.IP, function(){
+   console.log("The Marsil SA server has started !!!"); 
+});
